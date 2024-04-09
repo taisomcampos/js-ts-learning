e3cd871a6f445621bd4fb050e53fb01234e2ede5
Taisom Campos <taisomcampos@outlook.com>
2024-04-09 16:34:03 -0300

    Variáveis com let

---
+++ Aula 06/index.js
@@ -0,0 +1,8 @@
+// Não se pode criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não se pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Ultiliza-se camelCase
+// Variáveis são case-sensitive, ou seja, letra maiúscula e minúscula fazem diferença
+// Não se pode redeclarar variáveis com let
+// NÃO ULTILIZAR VAR E SIM LET
