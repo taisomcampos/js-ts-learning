bad9c6170208edcc7f8fa92582477953685a7dac
Taisom Campos <taisomcampos@outlook.com>
2024-04-12 19:39:06 -0300

    Operadores: Aritméticos, de Atribuição e Incremento

---
+++ Aula 11/index.js
@@ -0,0 +1,26 @@
+/*
+Ordem de precedência dos Operadores Aritméticos
+1º. ()
+2º. **
+3º. * / %
+
+Operadores de Incremento e Decremento
+Incremento ++
+Decremento --
+*/
+
+/*
+let num1 = 0;
+num1 += 2;
+num1 += 2;
+num1 += 2;
+num1 += 2;
+console.log(num1);
+*/
+
+let contador = 0;
+contador += 5;
+contador += 5;
+contador += 5;
+contador += 5;
+console.log(contador);
