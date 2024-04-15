155567bd7704addb1cd27d23aef58e99bec1fc3b
Taisom Campos <taisomcampos@outlook.com>
2024-04-15 20:59:31 -0300

    Tipos de Dados Primitivos

---
+++ JavaScript Básico - Para iniciantes/Aula 10/index.js
@@ -0,0 +1,18 @@
+const nome = 'Frodo'; // String
+const sobrenome = "Bolseiro"; // String
+const cidade = `Condado`; // String
+const idade = 52; // Number
+const aniversario = 12.06; // Number
+let nomeTio; // Undefined: não aponta para local nenhum na memória
+const sobrenomeTio = null; // Nulo: não aponta para local nenhum na memória
+const viajem = true; // Boolean: true, false (mas vai bem além disso)
+
+/*
+A diferença entre null e undefined é que null representa a ausencia intencional de valor, ou seja, é um valor que eu mesmo posso atrubuir a uma variável para indicar que ela não possui nenhum valor válido.
+
+Já undefined representa a ausencia não intencional de valor. É o valor padrão atribuído a uma variável que foi declarada, mas não inicializada com um valor.
+
+Boolean serve, dentre muitas coisas, para mudar o fluxo da aplicação. É um tipo de dado lógico.
+
+Essas anotações não são as finais. São anotações em forma de rascunho do que vou aprendendo enquanto vejo a aula. Caso você esteja lendo, recomende que também pesquise por conta própria, onde seguramente você encontará informações mais detalhadas.
+*/
