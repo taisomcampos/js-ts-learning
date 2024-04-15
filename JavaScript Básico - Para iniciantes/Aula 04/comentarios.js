9784569a8bb4955c885d4f7c01d0b7b9b21bd7ee
Taisom Campos <taisomcampos@outlook.com>
2024-04-15 20:46:48 -0300

    Comentários de código

---
+++ JavaScript Básico - Para iniciantes/Aula 04/comentarios.js
@@ -0,0 +1,15 @@
+// Escrever um comentário
+console.log('Olá mundo!'); //Aqui tem outro comentário
+console.log('Linha 1');
+// console.log('Linha 2');
+
+/*
+console.log('Linha 3');
+console.log('Linha 4');
+*/
+
+// Comentário de linha
+
+/* 
+Comentário de bloco
+*/
