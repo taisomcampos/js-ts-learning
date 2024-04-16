ff71b95b5b7cbcb5c36d518f31b2d803e59806e5
Taisom Campos <taisomcampos@outlook.com>
2024-04-15 22:26:58 -0300

    Mais sobre strings

---
+++ JavaScript Básico - Para iniciantes/Aula 14/index.js
@@ -0,0 +1,11 @@
+/*               01234567  
+let umaString = "O rato roeu a roupa do rei de Roma.";
+
+console.log(umaString[8]);
+console.log(umaString.indexOf('texto'));
+console.log(umaString.lastIndexOf('texto'));
+console.log(umaString.length);
+console.log(umaString.indexOf('texto'));
+*/
+
+
