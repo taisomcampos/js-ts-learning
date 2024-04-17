e41d05356ef4e641bf6f8acbfab4a80deafc8d73
Taisom Campos <taisomcampos@outlook.com>
2024-04-16 21:45:14 -0300

    Objeto Math

---
+++ JavaScript Básico - Para iniciantes/Aula 16/index.js
@@ -0,0 +1,11 @@
+
+
+// let num2 = Math.floor(num1); *arredonda pra baixo*
+// let num2 = Math.ceil(num1); *arredonda pra cima
+// let num2 = Math.round(num1); *arredonda de forma automatico pro numero mais proximo
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); *pega o maior número da sequência
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); *pega o menor número da sequência
+// console.log(Math.random()); *gera um número aleatório
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(Math.PI); *valor de PI
+// console.log(Math.pow()); *potenciação, mas é melhor usar o operador dedicado pra isso
