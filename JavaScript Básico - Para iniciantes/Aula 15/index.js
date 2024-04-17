eca21f4dd33acacfae09dbd1c08f1cc5ae4b9a62
Taisom Campos <taisomcampos@outlook.com>
2024-04-16 21:43:30 -0300

    Mais sobre numbers

---
+++ JavaScript Básico - Para iniciantes/Aula 15/index.js
@@ -0,0 +1,16 @@
+// IEEE 754-2008
+let num1 = 10.58744;
+num1 = 0.7;
+num2 = 0.1;
+// num = ((num * 100) + (num2 * 100)) / 100;
+// num = ((num * 100) + (num2 * 100)) / 100;
+// num = ((num * 100) + (num2 * 100)) / 100;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+// num1 = parseFloat(num.toFixed(2));
+// num1 = Number(num.toFixed(2));
+console.log(num1);
+console.log(Number.isInteger(num1));
+console.log(num.toString());
+console.log(num.toString(2));
