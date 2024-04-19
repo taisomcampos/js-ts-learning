6b9172bb4176384fef0019caf4440e4e65f2500c
Taisom Campos <taisomcampos@outlook.com>
2024-04-19 20:22:17 -0300

    Exercícios com numbers e Math

--- JavaScript Básico - Para iniciantes/Aula 17/js/script.js
+++ JavaScript Básico - Para iniciantes/Aula 17/js/script.js
@@ -4,16 +4,9 @@ const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
 
-texto.innerHTML = '';
-
 texto.innerHTML += `<p>A raíz quadrada de <strong>${numero}</strong> é <strong>${Math.sqrt(numero)}</strong>.</p>`;
-
 texto.innerHTML += `<p><strong>${numero}</strong> é um número inteiro: <strong>${Number.isInteger(numero)}</strong>.</p>`;
-
 texto.innerHTML += `<p><strong>${numero}</strong> é NaN: <strong>${Number.isNaN(numero)}</strong>.</p>`;
-
 texto.innerHTML += `<p>Arredondando <strong>${numero}</strong> para baixo, seria: <strong>${Math.floor(numero)}</strong>.</p>`;
-
-texto.innerHTML += `<p>Arredondando <strong>${numero}</strong> para cima, seria: <strong>${Math.ceil(numero)}</strong>.</p>`;
-
-texto.innerHTML += `<p><strong>${numero}</strong>, com duas casas decimais, seria: <strong>${numero.toFixed(2)}</strong>.</p>`;
+texto.innerHTML += `<p>Arredondando <strong>${numero}</strong> para baixo, seria: <strong>${Math.ceil(numero)}</strong>.</p>`;
+texto.innerHTML += `<p><strong>${numero}</strong>, com duas casas decimais, seria: <strong>${Number(numero.toFixed(2))}</strong>.</p>`;
