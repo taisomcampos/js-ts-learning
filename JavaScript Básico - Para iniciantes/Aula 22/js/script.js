48ea8b10f3ac2bc6eb82cf75b1b5cb8bb926ac65
Taisom Campos <taisomcampos@outlook.com>
2024-04-30 22:32:28 -0300

    Exercício para função, array e objetos

---
+++ JavaScript Básico - Para iniciantes/Aula 22/js/script.js
@@ -0,0 +1,25 @@
+function meuEscopo() {
+    const form = document.querySelector('.formulario');
+    const resultado = document.querySelector('.resultado');
+    
+    const pessoas = [];
+
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        console.log(nome.value, sobrenome, peso, altura);
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo();
+
+// .value
+// Criar um objeto e colocar dentro de um aray
+// Fazer console.log dentro no array pra verificar quais foram os objetos colocados dentro do array
+// Adicionar o que foi enviado na div de resultado
