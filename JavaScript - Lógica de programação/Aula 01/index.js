f7d65efd5a7ea332c2d4e5c647fe4b3c699f93d4
Taisom Campos <taisomcampos@outlook.com>
2024-05-06 21:30:43 -0300

    Operadores de comparação

---
+++ JavaScript - Lógica de programação/Aula 01/index.js
@@ -0,0 +1,12 @@
+/*
+    Operadores de comparação
+        > maior que
+        >= maior que ou igual a
+        < menor que
+        <= menor que ou igual a a
+        == igualdade (somente checa valor)
+        === igualdade estrita (checa valor e tipo)
+        != diferente (somente checa valor)
+        !== diferente estrito (checa valor e tipo)
+*/
+
