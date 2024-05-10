c102744c873b0311252651d2e8b986a95c40d88c
Taisom Campos <taisomcampos@outlook.com>
2024-05-09 21:07:49 -0300

    Operadores Lógicos

---
+++ JavaScript - Lógica de programação/Aula 02/index.js
@@ -0,0 +1,6 @@
+/* 
+    Operadores Lógicos
+        && e (preciso de uma caneta azul e uma vermelha para escrever)
+        || ou (preciso de uma caneta azul ou de uma vermelha pra escrever)
+        ! não
+*/
