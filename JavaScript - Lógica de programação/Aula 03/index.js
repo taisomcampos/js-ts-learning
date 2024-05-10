8f4f3146250bc92ebdf80b6fb427bc52f5022ee4
Taisom Campos <taisomcampos@outlook.com>
2024-05-09 22:27:25 -0300

    Avaliação de Curto-Circuito (Short-Circuit)

--- JavaScript - Lógica de programação/Aula 03/index.js
+++ JavaScript - Lógica de programação/Aula 03/index.js
@@ -12,22 +12,4 @@ function falaOi() {
     return 'oi';
 }
 
-const vaiExecutar = false;
-
-console.log(vaiExecutar && falaOi());
 */
-
-/*
-const corUsuario = null;
-const corPadrao = corUsuario || 'preto';
-
-console.log(corPadrao);
-*/
-
-const a = 0;
-const b = null;
-const c = 'false';
-const d = false;
-const e = NaN; // #
-
-console.log(a || b || c || d || e);
