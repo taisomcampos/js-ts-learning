76cc005c31a3639a767b82d193b0951555fad232
Taisom Campos <taisomcampos@outlook.com>
2024-05-13 18:21:28 -0300

    Avaliação de Curto-Circuito (Short-Circuit)

--- JavaScript - Lógica de programação/Aula 03/index.js
+++ JavaScript - Lógica de programação/Aula 03/index.js
@@ -5,4 +5,28 @@
         '' "" ``
         null / undefined
         NaN
+
+    Exemplo 01  (&&)  
+        function falaOi() {
+            return 'Oi!';
+        }
+
+        let vaiExecutar = 'false';
+
+        console.log(vaiExecutar && falaOi());
+
+    Exemplo 02 (||)
+        const corUsuario = null;
+        const corPadrao = corUsuario || 'preto';
+
+        console.log(corPadrao);
+
+    Exemplo 03 (||)
+        const a = 0;
+        const b = null;
+        const c = 'false'; // esse é o que foi exibido
+        const d = false;
+        const e = NaN;
+
+        console.log(a || b || c || d || e);
 */
