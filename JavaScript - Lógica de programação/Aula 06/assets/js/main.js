f494e3f4b00ecc7a96af803b9df35c1f85173033
Taisom Campos <taisomcampos@outlook.com>
2024-05-23 22:18:05 -0300

    Exercício (unindo tudo aprendido até aqui e mais)

--- JavaScript - Lógica de programação/Aula 06/assets/js/main.js
+++ JavaScript - Lógica de programação/Aula 06/assets/js/main.js
@@ -1,6 +1,20 @@
 const formulario = document.querySelector('#formulario');
 
-formulario.addEventListener('submit', function(e) {
+formulario.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('Evento previnido.');
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
 }); // para o envio do formulário
+
+function criaP() {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado(msg) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    const p = criaP();
+}; // cria função para resultado
