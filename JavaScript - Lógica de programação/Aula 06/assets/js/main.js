2ce30000e5840e9ffb860db6e6e4302b468b1958
Taisom Campos <taisomcampos@outlook.com>
2024-05-27 20:38:04 -0300

    Exercício (unindo tudo aprendido até aqui e mais)

--- JavaScript - Lógica de programação/Aula 06/assets/js/main.js
+++ JavaScript - Lógica de programação/Aula 06/assets/js/main.js
@@ -6,15 +6,49 @@ formulario.addEventListener('submit', function (e) {
     const inputAltura = e.target.querySelector('#altura');
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
-}); // para o envio do formulário
+
+    if (!peso) {
+        setResultado('Peso inválido!', false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado('Altura inválida!', false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    console.log(imc, nivelImc);
+    // continua o código...
+});
+
+function getNivelImc(imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel = [5];
+    if (imc >= 34.9) return nivel = [4];
+    if (imc >= 29.9) return nivel = [3];
+    if (imc >= 24.9) return nivel = [2];
+    if (imc >= 18.5) return nivel = [1];
+    if (imc < 18.5) return nivel = [0];
+}
+
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
 
 function criaP() {
     const p = document.createElement('p');
     return p;
 }
 
-function setResultado(msg) {
+function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
     const p = criaP();
+    p.innerHTML = msg;
+    resultado.appendChild(p);
 }; // cria função para resultado
