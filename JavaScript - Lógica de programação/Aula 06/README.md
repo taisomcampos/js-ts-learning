8bf2dc2f85465b381cd2d14a8fed8832b5ba590f
Taisom Campos <taisomcampos@outlook.com>
2024-05-28 23:46:25 -0300

    Exercício (unindo tudo aprendido até aqui e mais)

---
+++ JavaScript - Lógica de programação/Aula 06/README.md
@@ -0,0 +1,27 @@
+# Exercício (unindo tudo aprendido até aqui e mais)
+
+Este exercício teve como finalidade testar todos os conhecimentos adquiridos até aqui, e um pouco mais. Confesso que nesse momento do meu aprendizado, tive certa dificuldade em resolve-lo, necessitando consultar o código do instrutor, para poder chegar até o final da tarefa.
+
+Foi criado aqui, uma tabela para calcular o IMC de uma pessoa, utilizando somente HTML, CSS e JavaScript.
+
+## Tela de início
+
+Nesta imagem, veremos a primeira tela do exercício proposto.
+
+![Screenshot](/JavaScript%20-%20Lógica%20de%20programação/Aula%2006/calculoimc.png)
+
+## Tela de início
+
+Na segunda imagem, poderemos ver um calculo de exemplo, dando um resultado positivo e, portanto, exibindo na tela uma mensagem com fundo verde.
+
+![Screenshot](/JavaScript%20-%20Lógica%20de%20programação/Aula%2006/valido.png)
+
+## Tela de início
+
+Na próxima imagem, poderemos ver o primeiro resultado, em vermelho, indicando que os dados de peso estão incorretos.
+
+![Screenshot](/JavaScript%20-%20Lógica%20de%20programação/Aula%2006/pesoinvalido.png)
+
+## Tela de início
+
+Na quarta e útima imagem, de forma similar à terceira, também poderemos ver um resultado em vermelho, indicando erro nos dados de altura.
--- JavaScript - Lógica de programação/Aula 06/assets/js/main.js
+++ JavaScript - Lógica de programação/Aula 06/assets/js/main.js
@@ -20,19 +20,21 @@ formulario.addEventListener('submit', function (e) {
     const imc = getImc(peso, altura);
     const nivelImc = getNivelImc(imc);
 
-    console.log(imc, nivelImc);
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
     // continua o código...
 });
 
 function getNivelImc(imc) {
     const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
 
-    if (imc >= 39.9) return nivel = [5];
-    if (imc >= 34.9) return nivel = [4];
-    if (imc >= 29.9) return nivel = [3];
-    if (imc >= 24.9) return nivel = [2];
-    if (imc >= 18.5) return nivel = [1];
-    if (imc < 18.5) return nivel = [0];
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
 }
 
 function getImc(peso, altura) {
@@ -48,7 +50,16 @@ function criaP() {
 function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
+
     const p = criaP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
+
     p.innerHTML = msg;
     resultado.appendChild(p);
 }; // cria função para resultado
