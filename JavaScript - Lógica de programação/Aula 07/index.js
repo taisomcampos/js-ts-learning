09074ea93155ffd6b30aa1e26192cc2e319d7978
Taisom Campos <taisomcampos@outlook.com>
2024-06-07 21:06:59 -0300

    Operação ternária

--- JavaScript - Lógica de programação/Aula 07/index.js
+++ JavaScript - Lógica de programação/Aula 07/index.js
@@ -1,7 +1,7 @@
 const pontuacaoUsuario = 1000;
-const controleUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+const resultado = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
 
-console.log(controleUsuario);
+console.log(resultado);
 
 /*
     Isso seria a mesma coisa que no código acima, utilizando o operador ternário
