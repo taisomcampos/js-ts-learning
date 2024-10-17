078455f6d1e941c82737710107e5eed344364cbe
Taisom Campos <taisomcampos@outlook.com>
2024-10-16 23:45:27 -0300

    Atribuição via desestruturação (Arrays)

---
+++ JavaScript - Lógica de programação/Aula 12/index1.js
@@ -0,0 +1,8 @@
+let a = 'A' // João
+let b = 'B' // Pedro
+let c = 'C' // Marcos
+
+let nomes = ['João', 'Pedro', 'Marcos'];
+[a, b, c] = nomes;
+
+console.log(a, b, c);
---
+++ JavaScript - Lógica de programação/Aula 12/index2.js
@@ -0,0 +1,5 @@
+const numeros = [100, 200, 300, 400, 500, 600, 700];
+const [um, dois, tres, ...resto] = numeros;
+
+console.log(um, dois, tres);
+console.log(resto);
