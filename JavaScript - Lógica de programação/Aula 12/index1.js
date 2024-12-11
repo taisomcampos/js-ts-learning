63c80ac6139ac28f90ddb18ff09f695f0ae8d92d
taisomcampos <taisomcampos@outlook.com>
2024-12-11 20:42:54 -0300

    Voltando

--- JavaScript - Lógica de programação/Aula 12/index1.js
+++ JavaScript - Lógica de programação/Aula 12/index1.js
@@ -1,8 +1,8 @@
 let a = 'A' // João
-let b = 'B' // Pedro
+let b = 'B' // Mateus
 let c = 'C' // Marcos
 
-let nomes = ['João', 'Pedro', 'Marcos'];
+let nomes = ['João', 'Mateus', 'Marcos'];
 [a, b, c] = nomes;
 
 console.log(a, b, c);
