f1c7084d6eeecfcee217800faf419e7bb7ec325f
taisomcampos <taisomcampos@outlook.com>
2025-01-14 21:14:36 -0300

    Atribuição via desestruturação (Objetos)

--- JavaScript - Lógica de programação/Aula 13/index.js
+++ JavaScript - Lógica de programação/Aula 13/index.js
@@ -1,13 +1,13 @@
-// Frodo Bolseiro, 33 anos, Rua do Bolsão, 315
+// Frodo Bolseiro, 33 anos, Rua do Bolsão, 312
 const pessoa = {
     nome: 'Frodo',
     sobrenome: 'Bolseiro',
-    idade: '33 anos',
+    idade: 33,
     endereco: {
         rua: 'Rua do Bolsão',
         numero: 312
     }
 };
 
-const { nome, sobrenome, idade, endereco: { rua, numero } } = pessoa;
-console.log(nome, sobrenome, idade, rua, numero);
+const { nome, sobrenome, idade, endereco: { rua, numero} } = pessoa;
+console.log(`Meu nome é ${nome} ${sobrenome}, tenho ${idade} anos e moro na ${rua}, nº ${numero}.`);
